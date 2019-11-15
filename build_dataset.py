c6e78b2eb44dd1f9fa9023f23844f3f392ed3ffb
Mario Rugeles <Mario.Rugeles@endava.com>
2019-11-15 17:51:34 -0500

    Refactor for building different datasets

--- build_dataset.py
+++ build_dataset.py
@@ -1,6 +1,74 @@
 import data_utils
+import pandas as pd
 
+
+
+def to_year(value_range):
+    if(value_range == 'no_cie10'):
+        return -1
+    
+    num = int(value_range[:3])
+    unit = value_range[-1]
+    
+    map_unit = {
+        'A': num,
+        'M': num / 12,
+        'D': num / 365,
+        'H': num / 8760
+    }
+    return int(map_unit[unit])
+
+def get_edad_inf(value):
+    value_range = {
+        '999': -1,
+        'de 0 a 5 años': 0,
+        'de 13 a 17 años': 13,
+        'de 18 a 24 años': 18,
+        'de 25 a 29 años': 25,
+        'de 30 a 37 años': 30,
+        'de 38 a 49 años': 38,
+        'de 50 a 62 años': 50,
+        'de 6 a 12 años': 6,
+        'mayor de 63 años': 63
+            }
+    return value_range[value]
+
+def get_edad_sup(value):
+    value_range = {
+        '999': -1,
+        'de 0 a 5 años': 5,
+        'de 13 a 17 años': 17,
+        'de 18 a 24 años': 24,
+        'de 25 a 29 años': 29,
+        'de 30 a 37 años': 37,
+        'de 38 a 49 años': 49,
+        'de 50 a 62 años': 62,
+        'de 6 a 12 años': 12,
+        'mayor de 63 años': 120
+            }
+    return value_range[value]
+
+def in_range(row):
+    return row['AFEC_EDADR_INF'] >= row['LIMITE_INFERIOR_EDAD_Y'] and  row['AFEC_EDADR_SUP'] < row['LIMITE_SUPERIOR_EDAD_Y']
+
+def cie10_sexo(value):
+    if(value == 1 or value == 2):
+        return True
+    return False
+
+
+
+
+print(f'Joining pqrd datasets...')
 dataset = data_utils.get_dataset()
-print(f'dataset.shape: {dataset.shape}')
-
 dataset.to_csv('datasets/dataset.csv', index = False)
+
+print(f'Merging with CIE10 dataset...')
+cie10_df = pd.read_csv('datasets/CIE10.csv', sep = ';')
+cie10_df['DESCRIPCION_COD_CIE_10_04'] = cie10_df['DESCRIPCION_COD_CIE_10_04'].apply(lambda value: value.lower())
+dataset_cie10 = pd.merge(left = dataset, right = cie10_df, how = 'left', left_on='CIE_10', right_on='DESCRIPCION_COD_CIE_10_04')
+
+
+print(f'dataset_cie10.shape: {dataset_cie10.shape}')
+
+dataset_cie10.to_csv('datasets/dataset_cie10.csv', index = False)
---
+++ build_model.py
@@ -0,0 +1,56 @@
+import sys
+import numpy as np
+import pandas as pd
+from time import time
+import matplotlib.pyplot as plt
+from IPython.display import display 
+from sklearn.metrics import fbeta_score
+import model_utils as model_utils
+import warnings
+warnings.filterwarnings('ignore')
+
+import json
+pd.set_option('display.max_colwidth', -1)
+def build_model(path_dataset, path_results):
+    print("Load dataset...")
+
+    with open('best_config.json') as json_data_file:
+        config = json.load(json_data_file)[0]
+    print(config)
+    seed = int(config['seed'])
+    test_size = config['test_size']
+
+    dataset = pd.read_csv(path_dataset)
+
+    labels = dataset[['RIESGO_VIDA']]
+    features = dataset.drop(['RIESGO_VIDA'], axis = 1)
+
+    print("Split dataset for training / test")
+    # Import train_test_split
+    from sklearn.model_selection import train_test_split
+
+    # Split the 'features' and 'labels' data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size = test_size, random_state = seed, stratify=labels)
+
+    # Show the results of the split
+    print("features_final set has {} samples.".format(features.shape[0]))
+    print("Training set has {} samples.".format(X_train.shape[0]))
+    print("Testing set has {} samples.".format(X_test.shape[0]))
+
+
+    print("Training models...")
+    classifiers = model_utils.init_classifiers(seed)
+
+    # Collect results on the learners
+    dfResults = pd.DataFrame(columns=['learner', 'train_time', 'pred_time', 'f_test', 'f_train'])
+
+    for clf in list(classifiers.values()):
+        clf_name = clf.__class__.__name__ 
+        clf, dfResults = model_utils.train_predict(clf, 2, X_train, y_train, X_test, y_test, dfResults)
+
+    dfResults = dfResults.sort_values(by=['f_test'], ascending = False)
+    dfResults.to_csv(path_results, index = False)
+
+
+if __name__ == '__main__':
+    build_model(sys.argv[1], sys.argv[2])
